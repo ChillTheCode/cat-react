3e922cfd098ecfd690502809a67cdce1389dcacc
ChillTheCode <edwin.daniel@ui.ac.id>
2024-07-14 16:19:34 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-function App() {
+const Pet = () => {
+  const handleClickYes = () => {
+    alert("haloo ara");
+  };
+
+  const handleClickNo = () => {
+    alert("Give this link to ara plz :)");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <div style={{ marginBottom: "20px" }}>
+        <img
+          src="./testcat.png"
+          alt="cat"
+          width="400"
+          style={{ alignSelf: "center" }}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "200px",
+        }}
+      >
+        <button onClick={handleClickYes} style={{ marginRight: "10px" }}>
+          Yes
+        </button>
+        <button onClick={handleClickNo} style={{ marginLeft: "10px" }}>
+          No
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  return <Pet />;
+};
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
+
 
 export default App;
